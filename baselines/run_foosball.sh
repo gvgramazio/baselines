53bf96cdc48221da9d659738f3f66b9ac2135f45
ggramazio <giusva90.subscriptions@gmail.com>
2018-12-02 19:35:35 +0100

    Add foosball runner script

---
+++ baselines/run_foosball.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+for seed in $(seq 0 2); do
+  OPENAI_LOG_FORMAT=csv,log,stdout
+  OPENAI_LOGDIR=$HOME/logs/foosball-ppo-hp/b512-$seed
+  nohup ./xvfb-run-safe -s "-screen 0 800x600x24" python3 -m baselines.run \
+    --alg=ppo2 \
+    --env=Foosball_sp-v0 \
+    --network=mlp \
+    --num_timesteps=1e7 \
+    --num_env=2 \
+    --seed=$seed \
+    --nsteps=512 \
+    --num_hidden=256 \
+    --num_layers=4 \
+    --value_network=copy \
+    --save_path=models/foosball/b512/foosball1M_ppo2 > foosball_512_$seed.out & disown;
+done
+
+for seed in $(seq 0 2); do
+  OPENAI_LOG_FORMAT=csv,log,stdout
+  OPENAI_LOGDIR=$HOME/logs/foosball-ppo-hp/b1024-$seed
+  nohup ./xvfb-run-safe -s "-screen 0 800x600x24" python3 -m baselines.run \
+    --alg=ppo2 \
+    --env=Foosball_sp-v0 \
+    --network=mlp \
+    --num_timesteps=1e7 \
+    --num_env=2 \
+    --seed=$seed \
+    --nsteps=1024 \
+    --num_hidden=256 \
+    --num_layers=4 \
+    --value_network=copy \
+    --save_path=models/foosball/b1024/foosball1M_ppo2 > foosball_1024_$seed.out & disown;
+done
